4cd703346b26159e9a412ecc71549cd47deef615
Nikhil Butani <36304254+nikhilbuts@users.noreply.github.com>
2021-05-16 11:26:18 +0530

    Update validate.js

--- assets/vendor/php-email-form/validate.js
+++ assets/vendor/php-email-form/validate.js
@@ -51,7 +51,7 @@
 
   function php_email_form_submit(thisForm, action, formData) {
     fetch(action, {
-      method: 'POST',
+      method: 'GET',
       body: formData,
       headers: {'X-Requested-With': 'XMLHttpRequest'}
     })
